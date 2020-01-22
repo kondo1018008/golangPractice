3365450b70102e8e619c8b780745640aa80cf21e
ShogoKondo <koiru4423syougokondou@yahoo.co.jp>
2020-01-23 08:42:47 +0900

    [add]contlrollerとserverの内容を追加、mainを改変

--- main.go
+++ main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"github.com/kondo1018008/golangPractice/db"
+	"github.com/kondo1018008/golangPractice/server"
 )
 
 func main() {
-    r := gin.Default()
-    r.GET("/", func(c *gin.Context) {
-        c.String(200, "Hello, World")
-	})
 	db.Init()
-	r.Run()
-	db.	Close();
+	server.Init()
 }
